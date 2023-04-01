db49cb89ad236c869b8c283a1641dfbb1f948e4f
Gaston Romero <107086703+GastonRomeroo@users.noreply.github.com>
2023-04-01 15:40:01 +0000

    internet de miercoles

--- src/Components/Body/ItemData/ListProducts/ItemList.jsx
+++ src/Components/Body/ItemData/ListProducts/ItemList.jsx
@@ -2,12 +2,16 @@ import { Item } from "../Item";
 
 export const ItemList = ({ items }) => {
 
-    const primeraLetraMayuscula = `${items[0].category[0].toUpperCase()}${items[0].category.slice(1)}`
+    //const primeraLetraMayuscula = `${items[0].category[0].toUpperCase()}${items[0].category.slice(1)}`
+    const primeraLetraMayuscula = `${items[0]?.category?.[0].toUpperCase()}${items[0]?.category?.slice(1)}`
+    console.log(primeraLetraMayuscula)
+
     
     return (
         <div>
             <div>
                 <p className="paraphStyle"><b>{primeraLetraMayuscula}</b></p>
+                
             </div>
             <div className="d-inline-flex justify-content-center flex-wrap">
                 {
--- src/Components/Body/ItemData/SectionService/Services.jsx
+++ src/Components/Body/ItemData/SectionService/Services.jsx
@@ -4,12 +4,21 @@ import ServiceJson from "../../../../BBDD/services.json"
 const Services = () => {
     console.log(ServiceJson);
     return (
-        <div className="card mb-3">
-            <div className="row g-0">
-                <h5 className="card-title"> {ServiceJson.img} </h5>
-            </div>
+        <div>
+            {ServiceJson.map(service => (
+                <div className="card mb-3" key={service.index}>
+                    <div className="row g-0">
+                        <h5 className="card-title">{service.title}</h5>
+                        <div className="card-body">
+                            <p className="card-text">{service.details}</p>
+                            <img src={service.img} alt={service.title} />
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
 
     )
 }
 
+export default Services
